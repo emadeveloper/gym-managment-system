791b83a2826924a01da0d1e29d893cc302c65bec
Emanuel Martinez <yinproducciones@gmail.com>
2025-11-14 15:47:09 -0300

    feat(application): assign default Role.USER during user registration

--- src/main/java/com/backend/application/port/in/command/RegisterUserCommand.java
+++ src/main/java/com/backend/application/port/in/command/RegisterUserCommand.java
@@ -3,4 +3,4 @@ package com.backend.application.port.in.command;
 import com.backend.domain.valueobject.Email;
 import com.backend.domain.valueobject.Role;
 
-public record RegisterUserCommand (Email email, String password, Role role){}
+public record RegisterUserCommand (Email email, String password){}
